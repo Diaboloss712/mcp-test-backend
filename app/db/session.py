6797186dbe8745f251e84f63f9c62d2a5ab2c10b
Ian <ijae0611@gmail.com>
2025-05-08 17:37:24 +0900

    feature2

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://iinje@localhost/mcp_db"
+DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost:5432/mcp_test"
+#postgresql+asyncpg://iinje@localhost/mcp_db   for Mac 
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
