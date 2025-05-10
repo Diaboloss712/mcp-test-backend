4b33825e2b9f427f33bc5a8355e72522e4efc3df
Diaboloss712 <diaboloss217+github@gmail.com>
2025-05-10 23:59:11 +0900

    init: app settings

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.v1 import problem, category, comment, review, user, auth
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from dotenv import load_dotenv
 import os
@@ -8,7 +9,19 @@ load_dotenv()
 SESSION_SECRET_KEY = os.getenv("SESSION_SECRET_KEY")
 
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key=SESSION_SECRET_KEY)
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=SESSION_SECRET_KEY
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router, prefix="/api/v1/users", tags=["Users"])
 app.include_router(auth.router, prefix="/api/v1/oauth2", tags=["OAuth2"])
